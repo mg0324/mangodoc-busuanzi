2d9d2ec366bb7f076840c4fb35808aeae18dc39f
mango mac <1092017732@qq.com>
2023-04-02 17:20:35 +0800

    发布1.1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+# mangodoc-busuanzi
+mangodoc busuanzi plugin for show visit，集成[busuanzi](https://busuanzi.ibruce.info/)插件，显示网站访问量情况！
+
+[![npm](https://img.shields.io/npm/v/mangodoc-plugin-toc.svg?style=flat-square)](https://www.npmjs.com/package/mangodoc-busuanzi)
+
+![示例](demo.png)
+
+
+## Usage
+1. Configure mangodoc-busuanzi:(配置 ）
+
+    ```html
+    <script>
+    window.$mangodoc = {
+      // 开启busuanzi
+      busuanzi: true,
+    </script>
+    ```
+
+2. Insert script into mangodoc document:
+
+    ```html
+    <script src="https://unpkg.com/mangodoc-busuanzi@1.1.0/dist/mangodoc-busuanzi.min.js"></script>
+    ```
---
+++ src/main.js
@@ -0,0 +1,28 @@
+function renderBusuanzi(){
+    if(busuanziConfig()){
+        var sc = document.createElement("script");
+        sc.src = "//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js";
+        sc.async = "async";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(sc, s);
+        console.info("[mangodoc-busuanzi] enable busuanzi success!")
+    }else{
+        console.info("[mangodoc-busuanzi] not enable busuanzi!")
+    }
+}
+function busuanziConfig(){
+    return window.$mangodoc.busuanzi;
+}
+export default {
+    afterEach(html,next){
+        // console.info("demo afterEach:"+html);
+        if(busuanziConfig()){
+            var busuanzi = `<div style="text-align:center;color:gray;">已有<span class="busuanzi-value" id="busuanzi_value_site_uv"></span>人访问 <span id="busuanzi_container_site_pv">总访问量<span id="busuanzi_value_site_pv"></span>次</span></div>`;
+            html += busuanzi;
+        }
+        next(html);
+    },
+    doneEach(){
+        renderBusuanzi();
+    }
+}
